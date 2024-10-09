27140d834ed17f4be6c624ef7dfde5e4c014fa44
EL <113089132+MSulaeman@users.noreply.github.com>
2024-10-09 13:59:04 +0700

    Update home.php

--- sr12/home.php
+++ sr12/home.php
@@ -1,41 +1,88 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
     <link href="https://cdn.jsdelivr.net/npm/daisyui@4.12.12/dist/full.min.css" rel="stylesheet" type="text/css" />
 </head>
-<body>
 
-    <!-- Features Section -->
+<body>
+    <!-- NAVBAR -->
+    <div class="navbar bg-base-100 rounded-lg bg-teal-400 ">
+        <div class="flex-1 text-white text">
+            <p class="text-2xl">SR12<span class="text-lg">Lite</span></p>
+        </div>
+        <div class="flex-none">
+            <div class="dropdown dropdown-end">
+                <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+                    <div class="indicator">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                        </svg>
+                    </div>
+                </div>
+                <div tabindex="0" class="card card-compact dropdown-content bg-base-100 z-[1] mt-3 w-52 shadow">
+                    <div class="card-body">
+                        <span class="text-lg font-bold">8 Items</span>
+                        <span class="text-info">Subtotal: $999</span>
+                        <div class="card-actions">
+                            <button class="btn btn-primary btn-block">View cart</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="dropdown dropdown-end">
+                <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+                    <div class="w-10 rounded-full">
+                        <img alt="Tailwind CSS Navbar component"
+                            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                    </div>
+                </div>
+                <ul tabindex="0"
+                    class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                    <li>
+                        <a class="justify-between">
+                            Profile
+                        </a>
+                    </li>
+                    <li><a>Settings</a></li>
+                    <li><a href="login.php">Logout</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
     <section class="bg-white py-20">
-    <div class="card card-compact bg-base-100 w-96 shadow-xl">
-        <figure>
-            <img
-            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-            alt="Shoes" />
-        </figure>
-        <div class="card-body">
-            <h2 class="card-title">Shoes!</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div class="card card-compact bg-base-100 w-96 shadow-xl">
+            <figure>
+                <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" alt="Shoes" />
+            </figure>
+            <div class="card-body">
+                <h2 class="card-title">Shoes!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
                 <div class="card-actions justify-end">
                     <button class="btn btn-primary">Buy Now</button>
                 </div>
+            </div>
         </div>
-    </div>
     </section>
 
     <!-- Call to Action Section -->
     <section class="bg-gray-200 py-20 flex">
         <div class="container mx-auto p-4 pt-6 md:p-6 lg:p-12">
-            <button class="bg-green-500  text-white font-bold py-2 px-4 rounded"><a href="daftar.php">Sign Up</a></button>
+            <button class="bg-green-500  text-white font-bold py-2 px-4 rounded"><a href="daftar.php">Sign
+                    Up</a></button>
         </div>
         <div class="container mx-auto p-4 pt-6 md:p-6 lg:p-12">
             <button class="bg-red-600  text-white font-bold py-2 px-4 rounded"><a href="login.php">Logout</a></button>
         </div>
-        
+
     </section>
     <script src="https://cdn.tailwindcss.com"></script>
 </body>
+
 </html>
