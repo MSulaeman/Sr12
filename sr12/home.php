286877af8a8c31186f752f29c4b694a052bbf67c
EL <113089132+MSulaeman@users.noreply.github.com>
2024-10-11 09:34:28 +0700

    a

--- sr12/home.php
+++ sr12/home.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/daisyui@4.12.12/dist/full.min.css" rel="stylesheet" type="text/css" />
 </head>
 
-<body style="height: auto;">
+<body class="h-96">
     <!-- NAVBAR -->
     <nav class="navbar bg-base-100  bg-transparent ">
         <div class="flex-1 text-teal-400 bg-red italic font-mono font-bold">
@@ -28,10 +28,12 @@
                 </div>
                 <div tabindex="0" class="card card-compact dropdown-content bg-base-100 z-[1] mt-3 w-60 shadow">
                     <div class="card-body">
-                        <span class="text-lg font-bold">8 Items</span>
-                        <span class="text-info">Subtotal: $999</span>
-                        <div class="card-actions">
-                            <button class="btn btn-primary btn-block">View cart</button>
+                        <span class="text-lg font-bold">0 Items</span>
+                        <span class="text-info">Subtotal: $0</span>
+                        <div class="">
+                            <a href="keranjang.php" class="btn bg-teal-400 text-white hover:bg-teal-600" >
+                                <button type="submit">View Cart</button>
+                            </a>
                         </div>
                     </div>
                 </div>
@@ -67,7 +69,7 @@
                 <h2 class="card-title">Nike 102</h2>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate, blanditiis?</p>
                 <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Buy Now</button>
+                    <button class="btn bg-teal-400 text-white hover:bg-teal-600">Buy Now</button>
                 </div>
             </div>
         </div>
@@ -79,7 +81,7 @@
                 <h2 class="card-title">Nike 102</h2>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate, blanditiis?</p>
                 <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Buy Now</button>
+                    <button class="btn bg-teal-400 text-white hover:bg-teal-600">Buy Now</button>
                 </div>
             </div>
         </div>
@@ -91,7 +93,7 @@
                 <h2 class="card-title">Nike 102</h2>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate, blanditiis?</p>
                 <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Buy Now</button>
+                    <button class="btn bg-teal-400 text-white hover:bg-teal-600">Buy Now</button>
                 </div>
             </div>
         </div>
@@ -103,7 +105,7 @@
                 <h2 class="card-title">Nike 102</h2>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate, blanditiis?</p>
                 <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Buy Now</button>
+                    <button class="btn bg-teal-400 text-white hover:bg-teal-600">Buy Now</button>
                 </div>
             </div>
         </div>
@@ -112,33 +114,19 @@
 
 
     <div class="container mx-auto">
-        <div class="carousel w-full">
-            <div id="item1" class="carousel-item w-full">
+        <div class="hero bg-base-200 ">
+            <div class="hero-content flex-col lg:flex-row">
                 <img
-                    src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
-                    class="w-full" />
+                    src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+                    class="max-w-sm rounded-lg shadow-2xl" />
+                <div>
+                    <h1 class="text-5xl font-bold">Spiderman!</h1>
+                    <p class="py-6">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum soluta aliquid adipisci nam neque, voluptatem nemo esse eligendi omnis id et recusandae labore quis vitae! Molestias ipsa id laborum odit.
+                    </p>
+                    <button class="btn bg-teal-400 hover:bg-teal-600 text-white">Get Started</button>
+                </div>
             </div>
-            <div id="item2" class="carousel-item w-full">
-                <img
-                    src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
-                    class="w-full" />
-            </div>
-            <div id="item3" class="carousel-item w-full">
-                <img
-                    src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
-                    class="w-full" />
-            </div>
-            <div id="item4" class="carousel-item w-full">
-                <img
-                    src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
-                    class="w-full" />
-            </div>
-        </div>
-        <div class="flex w-full justify-center gap-2 py-2">
-            <a href="#item1" class="btn btn-xs">1</a>
-            <a href="#item2" class="btn btn-xs">2</a>
-            <a href="#item3" class="btn btn-xs">3</a>
-            <a href="#item4" class="btn btn-xs">4</a>
         </div>
     </div>
 
---
+++ sr12/keranjang.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>keranjang</title>
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@4.12.12/dist/full.min.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <!-- NAVBAR -->
+    <nav class="navbar bg-base-100  bg-transparent ">
+        <div class="flex-1 text-teal-400 bg-red italic font-mono font-bold">
+            <p class="text-3xl mx-auto">SR12<span class="text-lg">Lite</span></p>
+        </div>
+        <div class="flex-none">
+            
+            
+            </div>
+            <div class="dropdown dropdown-end">
+                <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+                    <div class="avatar placeholder">
+                        <div class="bg-neutral text-neutral-content w-8 rounded-full">
+                            <span class="text-xs">UI</span>
+                        </div>
+                    </div>
+                </div>
+                <ul tabindex="0"
+                    class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-60 p-2 shadow">
+                    <li>
+                        <a class="justify-between">Profile</a>
+                    </li>
+                    <li><a>Settings</a></li>
+                    <li><a href="login.php">Logout</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- NAVBAR END -->
+
+
+
+
+    <script src="https://cdn.tailwindcss.com"></script>
+</body>
+</html>
