7803928a148ac17bf79c931c68c70e50c9fd69c6
EL <113089132+MSulaeman@users.noreply.github.com>
2024-10-15 19:51:08 +0700

    a

--- sr12/home.php
+++ sr12/home.php
@@ -8,14 +8,75 @@
     <link href="https://cdn.jsdelivr.net/npm/daisyui@4.12.12/dist/full.min.css" rel="stylesheet" type="text/css" />
 </head>
 
-<body style="height: auto;">
+<body>
     <!-- NAVBAR -->
-    <nav class="navbar bg-base-100  bg-transparent ">
-        <div class="flex-1 text-teal-400 bg-red italic font-mono font-bold">
-            <p class="text-3xl my-auto">SR12<span class="text-lg">Lite</span></p>
+    <nav class="navbar bg-base-100 bg-transparent shadow-md">
+        <div class="navbar-start">
+            <!-- Dropdown for mobile -->
+            <div class="dropdown">
+                <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M4 6h16M4 12h8m-8 6h16" />
+                    </svg>
+                </div>
+                <ul tabindex="0" class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                    <li><a href="home.php">Home</a></li>
+                    <li>
+                        <a>Kategori</a>
+                        <ul class="p-2">
+                            <li><a>Submenu 1</a></li>
+                            <li><a>Submenu 2</a></li>
+                        </ul>
+                    </li>
+                    <li><a>About</a></li>
+                </ul>
+            </div>
+            <!-- Logo -->
+
+            <div class="text-4xl text-teal-500 font-mono italic">SR12<span class="text-lg">Lite</span></div>
         </div>
-        <div class="flex-none">
-            <input type="text" placeholder="Search" class="input input-bordered w-24 md:w-auto" />
+
+        <div class="navbar-center hidden lg:flex z-10   ">
+
+
+            <!-- Menu for desktop -->
+            <ul class="menu menu-horizontal px-1">
+                <li><a href="home.php">Home</a></li>
+                <li>
+                    <details>
+                        <summary>Kategori</summary>
+                        <ul class="p-2">
+                            <li><a>Submenu 1</a></li>
+                            <li><a>Submenu 2</a></li>
+                        </ul>
+                    </details>
+                </li>
+
+                <li><a>About</a></li>
+
+
+            </ul>
+        </div>
+
+        <div class="navbar-end flex-none">
+
+            <label class="input input-bordered flex items-center gap-2">
+                <input type="text" class="grow" placeholder="Search" />
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 16 16"
+                    fill="currentColor"
+                    class="h-4 w-4 opacity-70">
+                    <path
+                        fill-rule="evenodd"
+                        d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                        clip-rule="evenodd" />
+                </svg>
+            </label>
+
+
             <div class="dropdown dropdown-end">
                 <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
                     <div class="indicator">
@@ -26,12 +87,14 @@
                         </svg>
                     </div>
                 </div>
-                <div tabindex="0" class="card card-compact dropdown-content bg-base-100 z-[1] mt-3 w-60 shadow">
+                <div tabindex="0" class="card card-compact dropdown-content bg-base-100 z-[1] mt-3 w-60 shadow ">
                     <div class="card-body">
-                        <span class="text-lg font-bold">8 Items</span>
-                        <span class="text-info">Subtotal: $999</span>
-                        <div class="card-actions">
-                            <button class="btn btn-primary btn-block">View cart</button>
+                        <span class="text-lg font-bold">0 Items</span>
+                        <span class="text-info">Subtotal: $0</span>
+                        <div class="">
+                            <a href="keranjang.php" class="btn bg-teal-400 text-white hover:bg-teal-600">
+                                <button type="submit">View Cart</button>
+                            </a>
                         </div>
                     </div>
                 </div>
@@ -44,8 +107,7 @@
                         </div>
                     </div>
                 </div>
-                <ul tabindex="0"
-                    class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-60 p-2 shadow">
+                <ul tabindex="0" class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-60 p-2 shadow">
                     <li>
                         <a class="justify-between">Profile</a>
                     </li>
@@ -57,92 +119,81 @@
     </nav>
     <!-- NAVBAR END -->
 
-    <!-- CARD -->
-    <div class=" mx-2 my-auto py-20 gap-1 flex flex-wrap ">
-        <div class="card card-compact bg-base-100 w-60 shadow-xl mx-auto hover:scale-105 ">
-            <figure>
-                <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" alt="Shoes" />
-            </figure>
-            <div class="card-body">
-                <h2 class="card-title">Nike 102</h2>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate, blanditiis?</p>
-                <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Buy Now</button>
-                </div>
-            </div>
-        </div>
-        <div class="card card-compact bg-base-100 w-60 shadow-xl mx-auto hover:scale-105">
-            <figure>
-                <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" alt="Shoes" />
-            </figure>
-            <div class="card-body">
-                <h2 class="card-title">Nike 102</h2>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate, blanditiis?</p>
-                <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Buy Now</button>
-                </div>
-            </div>
-        </div>
-        <div class="card card-compact bg-base-100 w-60 shadow-xl mx-auto hover:scale-105">
-            <figure>
-                <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" alt="Shoes" />
-            </figure>
-            <div class="card-body">
-                <h2 class="card-title">Nike 102</h2>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate, blanditiis?</p>
-                <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Buy Now</button>
-                </div>
-            </div>
-        </div>
-        <div class="card card-compact bg-base-100 w-60 shadow-xl mx-auto hover:scale-105">
-            <figure>
-                <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" alt="Shoes" />
-            </figure>
-            <div class="card-body">
-                <h2 class="card-title">Nike 102</h2>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate, blanditiis?</p>
-                <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Buy Now</button>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- CARD END-->
+    <!-- CARD SECTION-->
 
+        <div class="mx-2 my-auto py-20 gap-1 flex flex-wrap ">
+            <div class="card card-compact bg-base-100 w-60 shadow-xl mx-auto hover:scale-105 ">
+                <figure>
+                    <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" alt="Shoes" />
+                </figure>
+                <div class="card-body">
+                    <h2 class="card-title">Nike 102</h2>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate, blanditiis?</p>
+                    <div class="card-actions justify-end">
+                        <button class="btn bg-teal-400 text-white hover:bg-teal-600">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+            <div class="card card-compact bg-base-100 w-60 shadow-xl mx-auto hover:scale-105 ">
+                <figure>
+                    <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" alt="Shoes" />
+                </figure>
+                <div class="card-body">
+                    <h2 class="card-title">Nike 102</h2>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate, blanditiis?</p>
+                    <div class="card-actions justify-end">
+                        <button class="btn bg-teal-400 text-white hover:bg-teal-600">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+            <div class="card card-compact bg-base-100 w-60 shadow-xl mx-auto hover:scale-105 ">
+                <figure>
+                    <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" alt="Shoes" />
+                </figure>
+                <div class="card-body">
+                    <h2 class="card-title">Nike 102</h2>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate, blanditiis?</p>
+                    <div class="card-actions justify-end">
+                        <button class="btn bg-teal-400 text-white hover:bg-teal-600">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+            <div class="card card-compact bg-base-100 w-60 shadow-xl mx-auto hover:scale-105 ">
+                <figure>
+                    <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" alt="Shoes" />
+                </figure>
+                <div class="card-body">
+                    <h2 class="card-title">Nike 102</h2>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate, blanditiis?</p>
+                    <div class="card-actions justify-end">
+                        <button class="btn bg-teal-400 text-white hover:bg-teal-600">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+            <!-- Add other card items similarly... -->
+        </div>
+        <!-- CARD END -->
 
-    <div class="container mx-auto">
-        <div class="carousel w-full">
-            <div id="item1" class="carousel-item w-full">
-                <img
-                    src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
-                    class="w-full" />
-            </div>
-            <div id="item2" class="carousel-item w-full">
-                <img
-                    src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
-                    class="w-full" />
-            </div>
-            <div id="item3" class="carousel-item w-full">
-                <img
-                    src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
-                    class="w-full" />
-            </div>
-            <div id="item4" class="carousel-item w-full">
-                <img
-                    src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
-                    class="w-full" />
+        <!-- HERO SECTION -->
+        <div class="container mx-auto">
+            <div class="hero bg-base-200">
+                <div class="hero-content flex-col lg:flex-row">
+                    <img src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+                        class="max-w-sm rounded-lg shadow-2xl" />
+                    <div>
+                        <h1 class="text-5xl font-bold">Spiderman!</h1>
+                        <p class="py-6">
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum soluta aliquid adipisci nam
+                            neque, voluptatem nemo esse eligendi omnis id et recusandae labore quis vitae! Molestias ipsa id
+                            laborum odit.
+                        </p>
+                        <button class="btn bg-teal-400 hover:bg-teal-600 text-white">Get Started</button>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="flex w-full justify-center gap-2 py-2">
-            <a href="#item1" class="btn btn-xs">1</a>
-            <a href="#item2" class="btn btn-xs">2</a>
-            <a href="#item3" class="btn btn-xs">3</a>
-            <a href="#item4" class="btn btn-xs">4</a>
-        </div>
-    </div>
 
-    <script src="https://cdn.tailwindcss.com"></script>
+        <script src="https://cdn.tailwindcss.com"></script>
 </body>
 
 </html>
---
+++ sr12/keranjang.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>keranjang</title>
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@4.12.12/dist/full.min.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=arrow_back" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=delete" />
+</head>
+
+<body>
+    <!-- NAVBAR -->
+    <nav class="navbar bg-base-100  bg-transparent ">
+        <a href="home.php">
+        <span class="material-symbols-outlined">
+            arrow_back
+        </span>
+        </a>
+        <div class="flex-1 text-teal-400 bg-red italic font-mono font-bold">
+            <p class="text-4xl mx-auto">SR12<span class="text-lg">Lite</span></p>
+        </div>
+        <div class="flex-none">
+
+
+        </div>
+        <div class="dropdown dropdown-end">
+            <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+                <div class="avatar placeholder">
+                    <div class="bg-neutral text-neutral-content w-8 rounded-full">
+                        <span class="text-xs">UI</span>
+                    </div>
+                </div>
+            </div>
+            <ul tabindex="0"
+                class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-60 p-2 shadow">
+                <li>
+                    <a class="justify-between">Profile</a>
+                </li>
+                <li><a>Settings</a></li>
+                <li><a href="login.php">Logout</a></li>
+            </ul>
+        </div>
+        </div>
+    </nav>
+    <!-- NAVBAR END -->
+
+    <!-- TABLE -->
+
+    <div class="overflow-x-auto flex">
+        <table class="table mx-auto">
+            <!-- head -->
+            <thead class="text-lg text-slate-700">
+                <tr>
+                    <th></th>
+                    <th>Produk</th>
+                    <th>Deskripsi</th>
+                    <th>Harga</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <!-- row 1 -->
+                <tr>
+                    <th>
+                        <label>
+                            <input type="checkbox" class="checkbox" />
+                        </label>
+                    </th>
+                    <td>
+                        <div class="flex items-center gap-3">
+                            <div class="avatar">
+                                <div class="mask mask-squircle h-12 w-12">
+                                    <img
+                                        src="https://img.daisyui.com/images/profile/demo/2@94.webp"
+                                        alt="Avatar Tailwind CSS Component" />
+                                </div>
+                            </div>
+                            <div>
+                                <div class="font-bold">Hart Hagerty</div>
+                                <div class="text-sm opacity-50">United States</div>
+                            </div>
+                        </div>
+                    </td>
+                    <td>
+                        Zemlak, Daniel and Leannon
+                        <br />
+                        <span class="badge badge-ghost badge-sm">Desktop Support Technician</span>
+                    </td>
+                    <td>Purple</td>
+                    <th>
+                        <button class="btn btn-ghost btn-xs" type="submit">
+                            <span class="material-symbols-outlined">delete</span>
+                        </button>
+                    </th>
+                </tr>
+                <!-- row 2 -->
+                <tr>
+                    <th>
+                        <label>
+                            <input type="checkbox" class="checkbox" />
+                        </label>
+                    </th>
+                    <td>
+                        <div class="flex items-center gap-3">
+                            <div class="avatar">
+                                <div class="mask mask-squircle h-12 w-12">
+                                    <img
+                                        src="https://img.daisyui.com/images/profile/demo/3@94.webp"
+                                        alt="Avatar Tailwind CSS Component" />
+                                </div>
+                            </div>
+                            <div>
+                                <div class="font-bold">Brice Swyre</div>
+                                <div class="text-sm opacity-50">China</div>
+                            </div>
+                        </div>
+                    </td>
+                    <td>
+                        Carroll Group
+                        <br />
+                        <span class="badge badge-ghost badge-sm">Tax Accountant</span>
+                    </td>
+                    <td>Red</td>
+                    <th>
+                        <button class="btn btn-ghost btn-xs" type="submit">
+                            <span class="material-symbols-outlined">delete</span>
+                        </button>
+                    </th>
+                </tr>
+                <!-- row 3 -->
+                <tr>
+                    <th>
+                        <label>
+                            <input type="checkbox" class="checkbox" />
+                        </label>
+                    </th>
+                    <td>
+                        <div class="flex items-center gap-3">
+                            <div class="avatar">
+                                <div class="mask mask-squircle h-12 w-12">
+                                    <img
+                                        src="https://img.daisyui.com/images/profile/demo/4@94.webp"
+                                        alt="Avatar Tailwind CSS Component" />
+                                </div>
+                            </div>
+                            <div>
+                                <div class="font-bold">Marjy Ferencz</div>
+                                <div class="text-sm opacity-50">Russia</div>
+                            </div>
+                        </div>
+                    </td>
+                    <td>
+                        Rowe-Schoen
+                        <br />
+                        <span class="badge badge-ghost badge-sm">Office Assistant I</span>
+                    </td>
+                    <td>Crimson</td>
+                    <th>
+                        <button class="btn btn-ghost btn-xs" type="submit">
+                            <span class="material-symbols-outlined">delete</span>
+                        </button>
+                    </th>
+                </tr>
+                <!-- row 4 -->
+                <tr>
+                    <th>
+                        <label>
+                            <input type="checkbox" class="checkbox" />
+                        </label>
+                    </th>
+                    <td>
+                        <div class="flex items-center gap-3">
+                            <div class="avatar">
+                                <div class="mask mask-squircle h-12 w-12">
+                                    <img
+                                        src="https://img.daisyui.com/images/profile/demo/5@94.webp"
+                                        alt="Avatar Tailwind CSS Component" />
+                                </div>
+                            </div>
+                            <div>
+                                <div class="font-bold">Yancy Tear</div>
+                                <div class="text-sm opacity-50">Brazil</div>
+                            </div>
+                        </div>
+                    </td>
+                    <td>
+                        Wyman-Ledner
+                        <br />
+                        <span class="badge badge-ghost badge-sm">Community Outreach Specialist</span>
+                    </td>
+                    <td>Indigo</td>
+                    <th>
+                        <button class="btn btn-ghost btn-xs" type="submit">
+                            <span class="material-symbols-outlined">delete</span>
+                        </button>
+                    </th>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <!-- TABLE END -->
+
+
+    <script src="https://cdn.tailwindcss.com"></script>
+</body>
+
+</html>
